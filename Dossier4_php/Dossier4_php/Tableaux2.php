8d00c76f1f8c69a314e15517b7b2418f7e2aa775
LEFEBVRE Kevin <lefebvrekevin2011@gmail.com>
2023-05-12 16:06:22 +0200

    PHP_Travail_KevinL

---
+++ Dossier4_php/Dossier4_php/Tableaux2.php
@@ -0,0 +1,9 @@
+<?php
+
+$facture["Janvier"] = 500;
+$facture["Février"] = 620;
+// $........
+$facture["Décembre"] = 300; 
+
+var_dump($facture);
+?>
---
+++ Dossier6_php/heure.php
@@ -0,0 +1,12 @@
+<?php
+$datePattern = "/^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$/";
+$date = "2015-12-06";
+if (preg_match($datePattern, $date))
+{
+    echo "Date ".$date." valide.<br>";
+}
+else
+{
+    echo "Date ".$date." erronée.<br>";
+}
+?>
