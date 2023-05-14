13ee82e2132e602f7f061452ac2c8283ea63aff9
LEFEBVRE Kevin <lefebvrekevin2011@gmail.com>
2023-05-14 17:21:06 +0200

    Green_Village

---
+++ Green_Garden/Index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+
+<link rel="stylesheet" href="CSS/Index.css">
+</head>
+<body>
+<form name="Accueil" id="Bloque_Recherche" method="POST" action="">
+<img src="Images/Maison.jpg" id="La_Maison" alt="Maison"/>
+<h2 id="Green">Village Green</h2>
+<input type="search" id="Recherche_Produit" name="Recherche"> <!--Ceci est une barre pour rechercher des produit!-->
+
+</form>
+</body>
+</html>
